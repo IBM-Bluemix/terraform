3357190f940933e16ec8d690ac755c85d8a72520
Tim Mulhern <tim.mulhern.contractor@weather.com>
2016-06-07 14:54:20 -0400

    Removed sl resources that have not been implemented in master. Modifed website to include link that points to swift provider from sl object storage page

--- builtin/providers/softlayer/config.go
+++ builtin/providers/softlayer/config.go
@@ -13,13 +13,9 @@ type Config struct {
 }
 
 type Client struct {
-	virtualGuestService                         softlayer.SoftLayer_Virtual_Guest_Service
-	sshKeyService                               softlayer.SoftLayer_Security_Ssh_Key_Service
-	productOrderService                         softlayer.SoftLayer_Product_Order_Service
-	dnsDomainResourceRecordService              softlayer.SoftLayer_Dns_Domain_ResourceRecord_Service
-	dnsDomainService                            softlayer.SoftLayer_Dns_Domain_Service
-	networkApplicationDeliveryControllerService softlayer.SoftLayer_Network_Application_Delivery_Controller_Service
-	securityCertificateService                  softlayer.SoftLayer_Security_Certificate_Service
+	virtualGuestService softlayer.SoftLayer_Virtual_Guest_Service
+	sshKeyService       softlayer.SoftLayer_Security_Ssh_Key_Service
+	productOrderService softlayer.SoftLayer_Product_Order_Service
 }
 
 func (c *Config) Client() (*Client, error) {
@@ -30,43 +26,11 @@ func (c *Config) Client() (*Client, error) {
 		return nil, err
 	}
 
-	networkApplicationDeliveryControllerService, err := slc.GetSoftLayer_Network_Application_Delivery_Controller_Service()
-
-	if err != nil {
-		return nil, err
-	}
-
 	sshKeyService, err := slc.GetSoftLayer_Security_Ssh_Key_Service()
 
-	if err != nil {
-		return nil, err
-	}
-
-	dnsDomainService, err := slc.GetSoftLayer_Dns_Domain_Service()
-
-	if err != nil {
-		return nil, err
-	}
-
-	dnsDomainResourceRecordService, err := slc.GetSoftLayer_Dns_Domain_ResourceRecord_Service()
-
-	if err != nil {
-		return nil, err
-	}
-
-	securityCertificateService, err := slc.GetSoftLayer_Security_Certificate_Service()
-
-	if err != nil {
-		return nil, err
-	}
-
 	client := &Client{
-		virtualGuestService:                         virtualGuestService,
-		sshKeyService:                               sshKeyService,
-		dnsDomainService:                            dnsDomainService,
-		dnsDomainResourceRecordService:              dnsDomainResourceRecordService,
-		networkApplicationDeliveryControllerService: networkApplicationDeliveryControllerService,
-		securityCertificateService:                  securityCertificateService,
+		virtualGuestService: virtualGuestService,
+		sshKeyService:       sshKeyService,
 	}
 
 	log.Println("[INFO] Created SoftLayer client")
---
+++ website/source/docs/providers/softlayer/r/object_storage_container.html.markdown
@@ -0,0 +1,12 @@
+---
+layout: "softlayer"
+page_title: "SoftLayer: object storage"
+sidebar_current: "docs-softlayer-resource-softlayer-object-storage-container"
+description: |-
+  Provides Softlayer Object Storage Container
+---
+
+# softlayer_object_storage_container
+
+For managing SoftLayer object storage containers and objects, please see
+the [Swift provider](/docs/providers/swift/index.html), since SoftLayer's object storage is an implementation of Swift object storage.
